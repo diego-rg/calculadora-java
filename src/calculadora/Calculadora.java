6b42bd2fa57d3f9b947cc49b026f459f1d14e5f8
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-04 19:23:10 +0100

    suma, resta, multiplicación, división, resto

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -9,8 +9,11 @@ package calculadora;
 import java.util.Scanner;
 
 /**
+ * @author Diego Caculadora controlada por teclado
+ */
+/**
  * @author Diego
- * Caculadora controlada por teclado
+ *
  */
 public class Calculadora {
 
@@ -24,9 +27,8 @@ public class Calculadora {
 		int opcion = 0;
 		Scanner sc;
 		sc = new Scanner(System.in);
-		
-		do {
 
+		do {
 			/**
 			 * Sacamos el menú en la consola
 			 */
@@ -39,6 +41,53 @@ public class Calculadora {
 			System.out.println("0. Salir");
 			System.out.print("Introduzca una opción válida: ");
 
-	}
+			/**
+			 * Recogemos el input del usuario (String) y lo transformamos en integer
+			 */
+			opcion = Integer.parseInt(sc.nextLine());
 
+			/**
+			 * Comprobamos el valor de opcion: si es menor a 0 o mayor a 5 no será válida.
+			 * Si está dentro de nuestros valores (0 a 5), solicitamos los siguientes datos
+			 */
+			if (opcion < 0 || opcion > 5) {
+				System.out.println("Opción no válida. Inténtelo de nuevo");
+			} else if (opcion != 0) {
+				System.out.println("\n");
+
+				float operando1, operando2;
+
+				System.out.print("Introduzca el primer operando: ");
+				operando1 = Float.parseFloat(sc.nextLine());
+
+				System.out.print("Introduzca el segundo operando: ");
+				operando2 = Float.parseFloat(sc.nextLine());
+
+				System.out.println("\n");
+				switch (opcion) {
+
+				case 1: // Suma
+					System.out.println("El resultado de la suma es " + (operando1 + operando2));
+					break;
+				case 2: // Resta
+					System.out.println("El resultado de la resta es " + (operando1 - operando2));
+					break;
+				case 3: // Multiplicación
+					System.out.println("El resultado de la multiplicación es " + (operando1 * operando2));
+					break;
+				case 4: // División
+					System.out.println("El resultado de la división es " + (operando1 / operando2));
+					break;
+				case 5: // Resto
+					System.out.println("El resto de dividir " + operando1 + " entre " + operando2 + " es " + (operando1 % operando2));
+					break;
+				}
+			}
+
+		} while (opcion != 0);
+		
+		sc.close();
+
+		System.out.println("Finalizando la ejecución de la calculadora");
+	}
 }
